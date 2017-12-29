ba94eec5be2905926e600c052ac91084188f40cc
Volha_Bystrova <volha_bystrova@epam.com>
2017-12-29 14:51:11 +0300

    Created base structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/*
 switch (process.env.NODE_ENV) {
     case 'prod':
     case 'production':
@@ -11,4 +12,47 @@ switch (process.env.NODE_ENV) {
     case 'development':
     default:
         module.exports = require('./config/webpack.dev')({env: 'development'});
+}
+*/
+
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // плагин минимизации
+module.exports = {
+    entry: {
+        'polyfills': './src/polyfills.ts',
+        'app': './src/main.ts'
+    },
+    output:{
+        path: path.resolve(__dirname, './dist'),     // путь к каталогу выходных файлов - папка public
+        publicPath: '/dist/',
+        filename: "[name].js"       // название создаваемого файла
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module:{
+        rules:[   //загрузчик для ts
+            {
+                test: /\.ts$/, // определяем тип файлов
+                use: [
+                    {                        loader: 'awesome-typescript-loader',
+                        options: { configFileName: path.resolve(__dirname, 'tsconfig.json') }
+                    } ,
+                    'angular2-template-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core/,
+            path.resolve(__dirname, 'src'), // каталог с исходными файлами
+            {} // карта маршрутов
+        ),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'polyfills']
+        }),
+        new UglifyJSPlugin()
+    ]
 }
